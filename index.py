29aa7fa07cdc1539c9925cb37187cd6af7eca1f7
Erwin Kodiat <750222+erwin-kodiat@users.noreply.github.com>
2023-05-22 19:38:29 +0700

    Update index.py - clean up commented

--- index.py
+++ index.py
@@ -1,22 +1,5 @@
 from flask import Flask
 import os
-
-### kalo mau RAW pake yang ini
-# import logging
-# FORMAT = ('%(asctime)s %(levelname)s [%(name)s] [%(filename)s:%(lineno)d] [dd.service=%(dd.service)s dd.env=%(dd.env)s dd.version=%(dd.version)s dd.trace_id=%(dd.trace_id)s dd.span_id=%(dd.span_id)s] - %(message)s')
-# logging.basicConfig(format=FORMAT)
-
-
-### kalo mau JSON pake yang ini
-# import logging
-# import json_log_formatter
-# json_handler = logging.StreamHandler() #FileHandler(filename='/tmp/my-log.json')
-# formatter = json_log_formatter.JSONFormatter()
-# json_handler.setFormatter(formatter)
-# logger = logging.getLogger()
-# logger.addHandler(json_handler)
-
-# logger.setLevel(logging.INFO)
     
 app = Flask(__name__)
 
